f3cba1ae4b4be7437c8c3f9e60f93f0572556b29
can hepkasar <can_hepkasar@hotmail.com>
2021-03-04 00:14:14 +0300

    Add project files.

---
+++ BasvuruManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void Basvuruyap(IKrediManager krediManager,List<ILoggerService> loggerService)
+        {
+            krediManager.Hesapla();
+            foreach (var logger in loggerService)
+            {
+                logger.Log();
+            }
+        }
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager tasitKrediManager= new TasitKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            basvuruManager.Basvuruyap(esnafKrediManager, new List<ILoggerService> {new DatabaseLoggerService(),new FileLoggerService() });
+            
+         
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager,konutKrediManager };
+          //  basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
